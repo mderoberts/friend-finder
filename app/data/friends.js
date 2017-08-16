9a20f5d00a5e3cf72a8ca2f100dddb63e234a512
MB DR <mderoberts@users.noreply.github.com>
2017-08-16 13:12:24 -0400

    added friends, cleaned up server.js

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+// Friends (DATA)
+
+var friends = [{
+  "name": "Emily Garcia",
+  "photo": "http://www.designskilz.com/random-users/images/imageF26.jpg",
+  "scores": []
+}, {
+  "name": "George Fuller",
+  "photo": "http://www.designskilz.com/random-users/images/imageM39.jpg",
+  "scores": []
+}, {
+  "name": "Janet Russell",
+  "photo": "http://www.designskilz.com/random-users/images/imageF40.jpg",
+  "scores": []
+},
+{
+  "name": "Emma Carter",
+  "photo": "http://www.designskilz.com/random-users/images/imageF20.jpg",
+  "scores": []
+},
+{
+  "name": "Tyler Morgan",
+  "photo": "http://www.designskilz.com/random-users/images/imageM15.jpg",
+  "scores": []
+}];
+
+module.exports = friends;
+
--- server.js
+++ server.js
@@ -1,37 +1,22 @@
+// Dependencies
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
+// Express setup
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-create.connection(function(err) {
-    if(err) throw err;
-    console.log("Connected with " + connection.threadId);
-});
-
-// Express data parsing to JSON
+// Parsing data to JSON with Express
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-
-// Friends (DATA)
-// =============================================================
-var friends = [{
-  "name": " "",
-  "photo": " "",
-  "scores": " []
-}, {
-  "name": " "",
-  "photo": " "",
-  "scores": " []
-}, {
-  "name": " "",
-  "photo": " "",
-  "scores": " []
-}];
+// Router
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+require("./app/data/friends")(app);
 
 // Listener
 app.listen(PORT, function() {
