a55c757ac76f2f03dfc3702351b38bbc3dbbf1ee
MB DR <mderoberts@users.noreply.github.com>
2017-08-16 10:30:59 -0400

    set up for routes, json

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,8 @@
+// 3. Your `htmlRoutes.js` file should include two routes:
+
+//    * A GET Route to `/survey` which should display the survey page.
+//    * A default USE route that leads to `home.html` which displays the home page. 
+
+app.get("/survey" function(req, res) {
+
+});
--- server.js
+++ server.js
@@ -3,10 +3,37 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 create.connection(function(err) {
     if(err) throw err;
     console.log("Connected with " + connection.threadId);
 });
 
+// Express data parsing to JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+// Friends (DATA)
+// =============================================================
+var friends = [{
+  "name": " "",
+  "photo": " "",
+  "scores": " []
+}, {
+  "name": " "",
+  "photo": " "",
+  "scores": " []
+}, {
+  "name": " "",
+  "photo": " "",
+  "scores": " []
+}];
+
+// Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
