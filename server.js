62f44cf78770bb9209e151968ec307df593d9df5
MB DR <mderoberts@users.noreply.github.com>
2017-08-16 00:38:59 -0400

    html setup

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 var PORT = 3000;
+
+create.connection(function(err) {
+    if(err) throw err;
+    console.log("Connected with " + connection.threadId);
+});
+
