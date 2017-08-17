d963364160ae866118f2a28906fd17a002cbbea8
MB DR <mderoberts@users.noreply.github.com>
2017-08-16 20:40:15 -0400

    set routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,6 +3,19 @@
 //    * A GET Route to `/survey` which should display the survey page.
 //    * A default USE route that leads to `home.html` which displays the home page. 
 
-app.get("/survey" function(req, res) {
+var path = require("path");
 
-});
+// Routing
+
+module.exports = function(app) {
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+
+  // If no matching route is found default to home
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+
+};
